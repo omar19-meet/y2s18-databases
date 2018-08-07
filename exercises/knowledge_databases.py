da9b99608a5ad061a667bdc5c17b9cf3f5509ec4
meet <omar19@meet.mit.edu>
2018-08-07 05:14:00 -0400

    lol

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -21,17 +21,41 @@ def query_all_articles():
 	knowledge=session.query(Knowledge).all()
 	return knowledge
 
-def query_article_by_topic():
-	pass
-
-def delete_article_by_topic():
-	pass
-
+def query_article_by_topic(name):
+	knowledge=session.query(Knowledge).filter_by(topic_name=name).all()
+	return knowledge
+def query_article_by_rating(threshold):
+	knowledge=session.query(Knowledge).filter(Knowledge.rating< threshold).all()
+	return knowledge
+def query_article_by_primary_key(id):
+	knowledge=session.query(Knowledge).filter_by(article_id=id).first()
+	return knowledge
+def delete_article_by_topic(name):
+	session.query(Knowledge).filter_by(article_topic=name).delete()
+	session.commit()
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
+def edit_article_rating(updated_rating, topic_name):
+	knowledge=session.query(Knowledge).filter_by(topic_name=topic_name).first()
+	knowledge.rating=updated_rating
+	session.commit()
+def delete_article_by_rating(threshold):
+	session.query(Knowledge).filter(Knowledge.rating<threshold).delete()
+	session.commit()
+def query_top_five():
+	b=0
+	knowledge=session.query(Knowledge).all()
 
-def edit_article_rating():
-	pass
-add_article("jazz","Louis Armstrong","The Louis Armstrong Foundation", 6)
-print(query_all_articles())
+	for i in knowledge:
+		for a in range(10,1,-1):
+			while b<5:
+				if i.rating==a:
+					b+=1
+					return knowledge
 
+
+#add_article("jazz","Louis Armstrong","The Louis Armstrong Foundation", 6)
+#edit_article_rating(9,"jazz")
+#print(query_all_articles())
+print(query_top_five())
